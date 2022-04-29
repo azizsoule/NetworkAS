5897e2b6b697dedfebc6802335fdc697ee2bac8a
Aziz Soulé <51265402+azizsoule@users.noreply.github.com>
2022-04-29 10:24:44 +0000

    manual drawing test

--- app/src/main/java/ci/miage/mob/networkAS/views/GraphView.kt
+++ app/src/main/java/ci/miage/mob/networkAS/views/GraphView.kt
@@ -1,8 +1,7 @@
 package ci.miage.mob.networkAS.views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import ci.miage.mob.networkAS.models.Graph
@@ -13,19 +12,37 @@ class GraphView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var graph: Graph = Graph()
+    val node1 = Node(x = 200f, y = 500f, color = Color.BLUE, label = "node1")
+    val node2 = Node(x = 50f, y = 200f, color = Color.RED, label = "node2")
+
+    var graph: Graph = Graph(
+        nodes = mutableListOf(node1, node2),
+        links = mutableListOf(Link(start = node1, end = node2, color = Color.GREEN, label = "Link"))
+    )
 
     private fun drawNode(node: Node, canvas: Canvas) {
+        val radius : Float = 20f
         val paint: Paint = Paint()
+
         paint.color = node.color
-        canvas.drawCircle(node.x, node.y, 100f, paint)
+        canvas.drawCircle(node.x, node.y, radius, paint)
+
+        paint.textSize = 24f
+        canvas.drawText(node.label, node.x - (radius + 5), node.y - (radius + 5), paint)
     }
 
     private fun drawLink(link: Link, canvas: Canvas) {
         val paint: Paint = Paint()
+        val path: Path = Path()
+
+        path.moveTo(link.start.x, link.start.y)
+        path.lineTo(link.end.x, link.end.y)
+        path.close()
+        //val pathLength : Float = PathMeasure(path, true).length
         paint.color = link.color
         paint.strokeWidth = 2f
-        canvas.drawLine(link.start.x, link.start.y, link.end.x, link.end.y, paint)
+        canvas.drawPath(path, paint)
+        //canvas.drawText(link.label, link.start.x + (pathLength/2), link.start.y + (pathLength/2), paint)
     }
 
     override fun onDraw(canvas: Canvas?) {
