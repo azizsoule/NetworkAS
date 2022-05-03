875ebcac47239a3ba63f2e86ea5e4eea2336604f
romaricano <jeannoelromaricano@gmail.com>
2022-05-03 00:29:50 +0000

    version sent to Foursov (with corrections)

--- app/src/main/java/ci/miage/mob/networkAS/activities/MainActivity.kt
+++ app/src/main/java/ci/miage/mob/networkAS/activities/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         graphView = findViewById<GraphView>(R.id.graph_view)
-        supportActionBar?.title = "Network Ano N\\'Ganza Jean-noel / Soulé Arémou Malick Aziz"
+        supportActionBar?.title = "Network Ano N'Ganza Jean-noel / Soulé Arémou Malick Aziz"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
